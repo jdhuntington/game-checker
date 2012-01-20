6ec8b6028f7a5fcb8d789c12f90bd85d1a994845
JD Huntington <jdhuntington@engineyard.com>
2012-01-19 18:12:22 -0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameChecker::Application.config.secret_token = '18dbe377b01083ed8668e24275ad31770dcadec2f4e5156001a3e063018f3ca95ed99f15adfa865ab5db73f46d9c82b4ad683c80014ae9e453ed5672b505d848'
