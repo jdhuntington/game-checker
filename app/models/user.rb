33fb2328c5fbeff3f8b697f7c7e553ac0c6c39be
JD Huntington <jdhuntington@engineyard.com>
2012-01-19 18:29:53 -0800

    passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+require 'digest/md5'
+
 class User < ActiveRecord::Base
+  validates_presence_of :username, :password
+
+  before_create :encrypt_password
+
+  def encrypt_password
+    self.password = User.encrypt(password)
+  end
+
+  def self.encrypt(password)
+    Digest::MD5.hexdigest(password)
+  end
+
+  def self.lookup(username, password)
+    find_by_username_and_password(username, encrypt(password))
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creating' do
+    it 'should encrypt the password' do
+      u = User.create!({ :username => 'jd', :password => 'password' })
+      User.find(u.id).password.should_not == 'password'
+    end
+  end
+
+  describe 'authentication' do
+    it 'should properly look up a user' do
+      u = User.create!({ :username => 'jd', :password => 'password' })
+      User.lookup('jd', 'password').should == u
+    end
+  end
 end
